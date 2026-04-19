3e28c1180050f0ae70511ee89725c288e5e10693
naveom16 <satchakan5@gmail.com>
2026-04-20 00:38:17 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -275,4 +275,4 @@ def handle_player_reconnected(player_id, session):
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5002)
+    socketio.run(app, host='0.0.0.0', port=5000)
